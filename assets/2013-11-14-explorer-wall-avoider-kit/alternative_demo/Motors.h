860194e56e80df058ca13a341a32fc9096e694e3
Danny Staple <danny@orionrobots.co.uk>
2015-11-07 15:07:03 +0000

    Wall avoider kit - demo and circuits.

---
+++ assets/2013-11-14-explorer-wall-avoider-kit/alternative_demo/Motors.h
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+
+class Motor {
+  public:
+    int enable;
+    int in1;
+    int in2;
+
+    void setup() {
+      //Set all the pins as outputs
+      pinMode(enable, OUTPUT);
+      pinMode(in1, OUTPUT);
+      pinMode(in2, OUTPUT);
+    }
+    
+    void forward(int speed) {
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+#ifndef DEBUG
+      analogWrite(enable, speed);
+#endif
+    }
+    
+    void backward(int speed) {
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+#ifndef DEBUG
+      analogWrite(enable, speed);          
+#endif
+    }
+    
+    void stop() {
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, LOW);
+      analogWrite(enable, 0);
+    }
+};
---
+++ assets/2013-11-14-explorer-wall-avoider-kit/alternative_demo/alternative_demo.ino
@@ -0,0 +1,36 @@
+#include "Motors.h"
+#include "DistanceSensor.h"
+
+//Define the devices
+Motor motor_right = {3, 4, 5};
+Motor motor_left = {10, 8, 9};
+
+SR04 sensor_left = {11, 12};
+SR04 sensor_right = {6, 7};
+
+void setup() {
+  motor_left.setup();
+  motor_right.setup();
+  sensor_left.setup();
+  sensor_right.setup();
+  motor_left.forward(255);
+  motor_right.forward(255);
+}
+
+#define SENSOR_AVOID_DIST 30
+
+void loop() {
+  if(sensor_left.readDist() < SENSOR_AVOID_DIST) {
+    motor_right.backward(255);
+    delay(200);
+  } else {
+    motor_right.forward(255);
+  }
+  if(sensor_right.readDist() < SENSOR_AVOID_DIST) {
+    motor_left.backward(255);
+    delay(100);
+  } else {
+    motor_left.forward(255);
+  }
+}
+
