6b6d5379dd2bf6a47b0d5585982eef41234c0014
Danny Staple <danny@orionrobots.co.uk>
2023-06-15 15:52:46 +0100

    Progress - matching post tags

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,37 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addLayoutAlias('page', 'layouts/page.html');
-    eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
-    eleventyConfig.addLayoutAlias('index', 'layouts/index.html');
-    eleventyConfig.addLayoutAlias('subject_page', 'layouts/subject_page.html');
-    eleventyConfig.addLayoutAlias('post', 'layouts/post.html');
-    eleventyConfig.addLayoutAlias('gallery', 'layouts/gallery.html');
-    eleventyConfig.addLayoutAlias('galleryitem', 'layouts/galleryitem.html');
-    eleventyConfig.addLayoutAlias('product', 'layouts/product.html');
+    // TODO: Make this list defined
     eleventyConfig.addLayoutAlias('autogallery', 'layouts/autogallery.html');
     eleventyConfig.addLayoutAlias('common', 'layouts/common.html');
+    eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
+    eleventyConfig.addLayoutAlias('gallery', 'layouts/gallery.html');
+    eleventyConfig.addLayoutAlias('galleryitem', 'layouts/galleryitem.html');
+    eleventyConfig.addLayoutAlias('index', 'layouts/index.html');
+    eleventyConfig.addLayoutAlias('page', 'layouts/page.html');
+    eleventyConfig.addLayoutAlias('post', 'layouts/post.html');
+    eleventyConfig.addLayoutAlias('product', 'layouts/product.html');
+    eleventyConfig.addLayoutAlias('subject_page', 'layouts/subject_page.html');
+
+    let $collectionApi = null;
+
+    eleventyConfig.addCollection("posts", function(collectionApi) {
+        $collectionApi = collectionApi;
+        return collectionApi.getFilteredByGlob("_posts/*.md");
+    });
+
+    // Setup a post_url liquid tag.
+    // it takes the filename (without extension) of a post in the _posts collection, and returns the permalink.
+    eleventyConfig.addLiquidTag("post_url", function(liquidEngine) {
+        return {
+            parse: function(tagToken, remainingTokens) {
+                this.name = tagToken.args;
+            },
+            render: function(scope, hash) {
+                const matching_posts = $collectionApi.getFilteredByGlob("_posts/" + this.name + ".md");
+                if (matching_posts.length == 0) {
+                    throw new Error("Could not find post with name " + this.name);
+                }
+                return matching_posts[0].url;
+            }
+        }
+    });
 };
